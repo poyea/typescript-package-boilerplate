cd260d0e37f62dd3637b21cc92d102eae31e9ef6
John Law <johnlaw.po@gmail.com>
2026-05-18 00:57:32 +0100

    Move to bun

--- test/echo.test.ts
+++ test/echo.test.ts
@@ -1,12 +1,12 @@
+import { describe, it, expect } from "bun:test";
 import echoThis from "../src/echo";
-import { expect } from "chai";
 
 describe("Function for processing strings", () => {
   it("should return it in a form with length less than 16", () => {
-    expect(echoThis("This is a very long string. I believe.")).to.equal(
+    expect(echoThis("This is a very long string. I believe.")).toEqual(
       "This is a very long string. I believe.".slice(0, 16)
     );
-    expect(echoThis("This is not")).to.equal("This is not");
-    expect(echoThis("")).to.equal("");
+    expect(echoThis("This is not")).toEqual("This is not");
+    expect(echoThis("")).toEqual("");
   });
 });
